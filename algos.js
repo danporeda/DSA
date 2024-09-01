38dda7655c9a10ebd0e2cb63c50f0bd9f24980af
Dan <danporeda@yahoo.com>
2024-09-01 19:35:06 -0400

    slice and splice

--- algos.js
+++ algos.js
@@ -89,3 +89,21 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+
+// Slice and Splice
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain 
+// the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  const slice1 = arr1.slice(0);
+  const slice2 = arr2.slice(0);
+
+  slice2.splice(n, 0, ...slice1);
+  return slice2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
